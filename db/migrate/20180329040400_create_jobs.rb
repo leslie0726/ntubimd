aa0f65b8b8db912176b19d13dc6dbf9d27b20e44
leslie_zou <event1511252@gmail.com>
2018-03-29 16:45:04 +0800

    init

---
+++ db/migrate/20180329040400_create_jobs.rb
@@ -0,0 +1,12 @@
+class CreateJobs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.string :content
+      t.date :publish_date
+      t.boolean :is_online
+
+      t.timestamps
+    end
+  end
+end
